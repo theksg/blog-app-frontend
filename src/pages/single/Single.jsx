1deeddce95541616aecb455118dd39602e4ef13f
Saksham <gsaksham2000@gmail.com>
2022-08-16 10:49:28 +0530

    Deleted sidebar code

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,5 @@
 import "./single.css"
 import SinglePost from "../../components/singlePost/SinglePost";
-import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Single() {
   return (
